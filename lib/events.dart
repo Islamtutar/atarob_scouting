1e80c04350f6647ed92717e6bcb1dd749e94ad4b
Deniz Tunç <creeperkafasiyt@gmail.com>
2022-08-07 16:43:10 +0300

    Organized some files and added search functionality

---
+++ lib/events.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+
+Future<List<Event>> fetchEvents() async {
+  final response = await http.get(
+    Uri.parse('https://www.thebluealliance.com/api/v3/events/2022'),
+    headers: {
+      "X-TBA-Auth-Key":
+          "lSKhhpNwQCVhq3LXPJFDffLGdXQFMZhyHKQp7qbCubNkyl2JOjo0gGPpWQdurXPk",
+    },
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    // print(response.body);
+    var eventList = jsonDecode(response.body) as List<dynamic>;
+    return eventList.map((e) => Event.fromJson(e)).toList();
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Event {
+  final String name;
+  final int year;
+  final String city;
+
+  const Event({
+    required this.name,
+    required this.year,
+    required this.city,
+  });
+
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+      city: json["city"],
+      name: json["name"],
+      year: json["year"],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'dart:convert';
-import 'dart:io';
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:learning_flutter/event_card.dart';
+
+import 'events.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,106 +18,68 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late Future<List<Event>> eventsData;
+  late String searchString;
 
   @override
   void initState() {
     super.initState();
 
     eventsData = fetchEvents();
+    searchString = "";
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.green,
-            title: const Text("My cool flutter app"),
-          ),
-          body: ListView.builder(
-            addAutomaticKeepAlives: true,
-            itemBuilder: (context, index) {
-              return FutureBuilder(
-                future: eventsData,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    Event event = (snapshot.data as List<Event>)[index];
-                    return Container(
-                      height: 100,
-                      margin: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                        color: Colors.lightBlue[50],
-                      ),
-                      child: Column(
-                        children: [
-                          Text(
-                            event.name,
-                            style: const TextStyle(
-                              fontSize: 15,
-                            ),
-                          ),
-                          Text(event.city)
-                        ],
-                      ),
-                    );
-                  } else {
-                    return const Center(
-                      child: SizedBox(
-                        width: 60,
-                        height: 60,
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  }
-                },
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text("My cool flutter app"),
+        ),
+        body: FutureBuilder(
+          future: eventsData,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<Event> events = snapshot.data as List<Event>;
+              List<Event> matchingEvents = events
+                  .where((event) => event.city.contains(searchString))
+                  .toList();
+              return Column(
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Search for city',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        searchString = value;
+                      });
+                    },
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: matchingEvents.length,
+                      itemBuilder: (context, i) {
+                        return EventCard(event: matchingEvents[i]);
+                      },
+                    ),
+                  ),
+                ],
               );
-            },
-          )),
-    );
-  }
-}
-
-Future<List<Event>> fetchEvents() async {
-  final response = await http.get(
-    Uri.parse('https://www.thebluealliance.com/api/v3/events/2022'),
-    headers: {
-      "X-TBA-Auth-Key":
-          "lSKhhpNwQCVhq3LXPJFDffLGdXQFMZhyHKQp7qbCubNkyl2JOjo0gGPpWQdurXPk",
-    },
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    // print(response.body);
-    var eventList = jsonDecode(response.body) as List<dynamic>;
-    return eventList.map((e) => Event.fromJson(e)).toList();
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class Event {
-  final String name;
-  final int year;
-  final String city;
-
-  const Event({
-    required this.name,
-    required this.year,
-    required this.city,
-  });
-
-  factory Event.fromJson(Map<String, dynamic> json) {
-    return Event(
-      city: json["city"],
-      name: json["name"],
-      year: json["year"],
+            } else {
+              return const Center(
+                child: CircularProgressIndicator.adaptive(),
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
